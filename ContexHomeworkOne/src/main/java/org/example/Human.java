2fa47cb184b578f0455e07491b6201f3c94a75fb
KostyaLens <124429244+KostyaLens@users.noreply.github.com>
2024-06-27 08:25:36 +0300

    начал делать первое дз

---
+++ ContexHomeworkOne/src/main/java/org/example/Human.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Human {
+    private Cat cat;
+    private Dog dog;
+    private Parrot parrot, parrot1;
+}
---
+++ ContexHomeworkOne/src/main/java/org/example/Parrot.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import lombok.Data;
+
+@Data
+public class Parrot implements Pet{
+    private String name;
+}
