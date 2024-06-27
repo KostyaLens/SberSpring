43e92a414274473331be9884e5de845bb441c65d
KostyaLens <124429244+KostyaLens@users.noreply.github.com>
2024-06-27 11:48:47 +0300

    вставил мэин

---
+++ ContexHomeworkOne/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        Human human = context.getBean(Human.class);
+        System.out.println(human.getParrot().getName());
+    }
+}
